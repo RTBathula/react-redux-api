f0f8d31a9886782357dd4f44d65243a4d08ab771
bathula <battu.network@gmail.com>
2017-04-09 09:23:50 -0300

    started working on details

---
+++ app/components/company/details/beneficials/beneficials.js
@@ -0,0 +1,127 @@
+import React, { PropTypes, Component } from 'react'
+import {Link} from 'react-router'
+
+//Css
+import layoutStyle from 'components/layout.css'
+import style from '../details.css'
+import infoStyle from './info.css'
+
+import LocalTime from 'components/helpers/localTime'
+
+class App extends Component { 
+
+   constructor(props) {
+    super(props);
+    this.state = {                
+    }
+  }
+
+  componentWillMount() {      
+  }
+
+  render() {
+    return (         
+        
+        <div className={style.detailsBx}>
+          <div className={"flex-row-start-start"}>
+            <div style={{"height":"20px"}} className={"vertical-center"}>
+              <span style={{"fontSize":"17px","fontWeight":"500"}}>Details</span>
+            </div>
+            <div style={{"height":"20px","marginLeft":"2px"}} className={"vertical-center"}>
+              <span style={{"color":"blue","textDecoration":"underline","cursor":"pointer"}}>(Edit)</span>
+            </div>
+          </div>
+          <div style={{"borderTop":"1px solid gray","margin":"6px 0px 6px 0px","width":"100%"}}>
+          </div>
+
+          <div>
+            <div className={"flex-row-start-start "+style.keyvalueWrap}>
+              <div className={style.key}>
+                <span>Company Id:</span>
+              </div>
+              <div className={style.value}>
+                <span>World</span>
+              </div>
+            </div>
+
+            <div className={"flex-row-start-start "+style.keyvalueWrap}>
+              <div className={style.key}>
+                <span>Company Name:</span>
+              </div>
+              <div className={style.value}>
+                <span>World</span>
+              </div>
+            </div>
+
+            <div className={"flex-row-start-start "+style.keyvalueWrap}>
+              <div className={style.key}>
+                <span>Company Address:</span>
+              </div>
+              <div className={style.value}>
+                <span>World</span>
+              </div>
+            </div>
+
+            <div className={"flex-row-start-start "+style.keyvalueWrap}>
+              <div className={style.key}>
+                <span>Company City:</span>
+              </div>
+              <div className={style.value}>
+                <span>World</span>
+              </div>
+            </div>
+
+            <div className={"flex-row-start-start "+style.keyvalueWrap}>
+              <div className={style.key}>
+                <span>Company Country:</span>
+              </div>
+              <div className={style.value}>
+                <span>World</span>
+              </div>
+            </div>
+
+            <div className={"flex-row-start-start "+style.keyvalueWrap}>
+              <div className={style.key}>
+                <span>Company Email:</span>
+              </div>
+              <div className={style.value}>
+                <span>World</span>
+              </div>
+            </div>
+
+            <div className={"flex-row-start-start "+style.keyvalueWrap}>
+              <div className={style.key}>
+                <span>Company Phone:</span>
+              </div>
+              <div className={style.value}>
+                <span>World</span>
+              </div>
+            </div>
+
+            <div className={"flex-row-start-start "+style.keyvalueWrap}>
+              <div className={style.key}>
+                <span>Created On:</span>
+              </div>
+              <div className={style.value}>
+                <span>World</span>
+              </div>
+            </div>
+
+            <div className={"flex-row-start-start "+style.keyvalueWrap}>
+              <div className={style.key}>
+                <span>Last Updated On:</span>
+              </div>
+              <div className={style.value}>
+                <span>World</span>
+              </div>
+            </div>
+
+          </div>
+
+        </div>              	  	
+    );
+  }
+}
+
+
+export default App;
--- app/components/company/details/details.js
+++ app/components/company/details/details.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router'
 import layoutStyle from 'components/layout.css'
 import style from './details.css'
 
-import CreateCompanyModal from '../createCompanyModal/createCompanyModal'
+import Maininfo from './info/info'
 import LocalTime from 'components/helpers/localTime'
 
 class App extends Component { 
@@ -23,6 +23,7 @@ class App extends Component {
     return (  
       <div className={layoutStyle.screenfull+' '+style.bodystrip+' horizontal-center'}>
         <div className={layoutStyle.screen980}>  
+          <Maininfo />
         </div>
       </div>              	  	
     );
